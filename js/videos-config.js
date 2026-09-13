aeea40389f679bd3a93395235a8683587f94dce0
jeanaviat <jean.aviat2@gmail.com>
2026-09-13 13:17:52 +0200

    Update 'retour-aux-sources' video ID

--- js/videos-config.js
+++ js/videos-config.js
@@ -11,5 +11,5 @@ const YOUTUBE_IDS = {
   "skopein-fobos": "3Ru_AoNsizE",
   "dust": "ys9dUExQYRs",
   "waiting-for-godot": "ATsQe4yhNO0",
-  "retour-aux-sources": ""
+  "retour-aux-sources": "w3fiEcUXYxo"
 };
