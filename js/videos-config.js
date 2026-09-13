63f0aea55f704bf9ccbd88ae7f832fbd2d0dabde
jeanaviat <jean.aviat2@gmail.com>
2026-09-13 12:45:00 +0200

    Update video ID for 'my-wife' entry

--- js/videos-config.js
+++ js/videos-config.js
@@ -7,7 +7,7 @@ const YOUTUBE_IDS = {
   "foreigner": "UZ3TbdSMw0sK_fTw",
   "crumbs-title": "q2N9NQEC5_GUt4hw",
   "crumbs-credits": "XxW7jcjrbZKP44zr",
-  "my-wife": "Y0m_pZ4__C59tGnc",
+  "my-wife": "M5geVM67VL4",
   "skopein-fobos": "",
   "dust": "QQXWzqGpiDlo9DkW",
   "waiting-for-godot": "5dmE4yasslpGhgAo",
